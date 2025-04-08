1ec11adf124c62b4394c43b741a9283846e04772
john kyony <jkyony@gmail.com>
2025-04-08 11:36:09 +0100

    fix cors issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"  # React app URL
+    origins "68.183.123.64"  # React app URL
 
     resource "*",
       headers: :any,
